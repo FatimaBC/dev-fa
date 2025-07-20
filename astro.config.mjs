2bf41ebb59eca2a8e69f8462c24f420fb95cd4de
FatimaBC <fatimabc_91@hotmail.com>
2025-07-20 13:17:48 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://valkyria-technologies.vercel.app',
+  integrations: [
+    react(),
+    tailwind(),
+  ],
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
+  build: {
+    inlineStylesheets: 'auto',
+    assets: '_astro',
+  },
+  output: 'static',
+  trailingSlash: 'never',
 });
